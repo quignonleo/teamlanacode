4ecb0628d0e7e953f3b3db9391a7cdc9e51d8728
Morgane PICOT <morgane.picot@mines-paristech.fr>
2021-01-29 13:19:14 +0100

    ddd

--- Map1.py
+++ Map1.py
@@ -12,24 +12,22 @@ clock = pg.time.Clock()
 font=pg.font.Font('freesansbold.ttf', 20)
 
 with open('map_finale.txt', 'r') as file:
-    board = []
+    list_map = []
     for line in file:
-        board.append([x for x in line if x != '\n'])
-    array_map = np.array(board)
-
-width, length = array_map.shape
+        list_map.append([x for x in line if x != '\n'])
+    n = len(list_map)
 
 running = True 
 while running:
     clock.tick(5)
 
-    for i in range(width):
-        for j in range(length):
-            if array_map[i, j] in ['.', '|', '-', '=', '+', '#', '@']:
-                text = font.render(array_map[i, j], 1, (255, 165, 0))
+    for i in range(n):
+        for j in range(len(list_map[i])):
+            if list_map[i][j] in ['.', '|', '-', '=', '+', '#', '@']:
+                text = font.render(list_map[i][j], 1, (255, 165, 0))
                 screen.blit(text, (j*20 + 100, i*20 + 150))
-            if array_map[i, j] == '@':
-                charact_pos = (j*20 + 100, i*20 + 150)
+            if list_map[i][j] == '@':
+                charact_pos = (j, i)
             
 
 
@@ -43,25 +41,24 @@ while running:
         
         if event.type == pg.KEYDOWN:
 
-    #on suppose que les éléments sont stockés dans un array nommé "screen"
-            if event.key == pg.K_LEFT and array_map[i-1, j] not in [' ', '|', '-']:
-                array_map[i-1, j], array_map[i, j] = array_map[i, j], array_map[i-1, j]
-                what_it_replaces, array_map[i, j] = array_map[i, j], what_it_replaces
-                charact_pos = (i-20, j)
+    #on suppose que les éléments sont stockés dans un list nommé "screen"
+            if event.key == pg.K_LEFT and list_map[i-1][j] not in ['a', '|', '-']:
+                list_map[i-1][j], list_map[i][j] = list_map[i][j], list_map[i-1][j]
+                what_it_replaces, list_map[i][j] = list_map[i][j], what_it_replaces
             
-            if event.key == pg.K_RIGHT and array_map[i+1, j] not in [' ', '|', '-']:
-                array_map[i+1, j], array_map[i, j] = array_map[i, j], array_map[i+1, j]
-                what_it_replaces, array_map[i, j] = array_map[i, j], what_it_replaces
+            if event.key == pg.K_RIGHT and list_map[i+1][j] not in ['a', '|', '-']:
+                list_map[i+1][j], list_map[i][j] = list_map[i][j], list_map[i+1][j]
+                what_it_replaces, list_map[i][j] = list_map[i][j], what_it_replaces
                 charact_pos = (i+1, j)
 
-            if event.key == pg.K_DOWN and array_map[i, j-1] not in [' ', '|', '-']:
-                array_map[i, j-1], array_map[i, j] = array_map[i, j], array_map[i, j-1]
-                what_it_replaces, array_map[i, j] = array_map[i, j], what_it_replaces
+            if event.key == pg.K_DOWN and list_map[i][j-1] not in ['a', '|', '-']:
+                list_map[i][j-1], list_map[i][j] = list_map[i][j], list_map[i][j-1]
+                what_it_replaces, list_map[i][j] = list_map[i][j], what_it_replaces
                 charact_pos = (i, j-1)
 
-            if event.key == pg.K_UP and array_map[i, j+1] not in [' ', '|', '-']:
-                array_map[i, j+1], array_map[i, j] = array_map[i, j], array_map[i, j+1]
-                what_it_replaces, array_map[i, j] = array_map[i, j], what_it_replaces
+            if event.key == pg.K_UP and list_map[i][j+1] not in ['a', '|', '-']:
+                list_map[i][j+1], list_map[i][j] = list_map[i][j], list_map[i][j+1]
+                what_it_replaces, list_map[i][j] = list_map[i][j], what_it_replaces
                 charact_pos = (i, j+1)
     
     pg.display.update()
