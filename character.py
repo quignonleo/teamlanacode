37e7e714331f6788b324ee86e34fbd6b4246d9a9
Léo Quignon <quignonleo@gmail.com>
2021-01-29 09:54:36 +0100

    fonctions d'équipement

--- character.py
+++ character.py
@@ -9,6 +9,29 @@ class character :
         self.inventory = inventory
         self.equipment = equipment 
         self.money = money 
-        self.attak = 1
-        self.defense = 0
+        self.atk = 1
+        self.deff = 0
+        self.name = ''
+
+    def name(self, name):
+        self.name = name
+    
+    def equip(self, item):
+        if item not in self.inventory :
+            return "Vous ne possédez pas cet item"
+        else : 
+            nature = item.nature
+            if self.equipment[nature] == []:
+                self.equipment[nature] = item
+                item.apply(self)
+            else :
+                item_porté = equipment[nature]
+                item_porté.unapply(self)
+                self.equipment[nature] = item
+                item.apply(self)
+            
+    
+
+                
+
 
